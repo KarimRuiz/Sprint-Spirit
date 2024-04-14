2760b64071d75cbf856726f8d2b8fe8f9330b1f5
karnedo <karnedo@proton.me>
2024-04-14 16:39:40 +0200

    commit added date to user profile sessions

--- app/src/main/java/com/example/sprintspirit/features/dashboard/profile/ui/ProfileRunAdapter.kt
+++ app/src/main/java/com/example/sprintspirit/features/dashboard/profile/ui/ProfileRunAdapter.kt
@@ -4,7 +4,9 @@ import android.graphics.Color
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.example.sprintspirit.R
 import com.example.sprintspirit.databinding.CardUserRunBinding
 import com.example.sprintspirit.features.run.data.RunData
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -12,8 +14,11 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.gms.maps.model.PolylineOptions
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ProfileRunAdapter(var runlist:List<RunData>) : RecyclerView.Adapter<ProfileRunAdapter.ProfileRunHolder>(){
 
@@ -48,14 +53,26 @@ class ProfileRunAdapter(var runlist:List<RunData>) : RecyclerView.Adapter<Profil
             val timeString = String.format("%d:%02d", (time*60).toInt()/60, (time*60).toInt()%60)
             binding.tvTimeValue.text = timeString + " min"
             binding.tvPaceValue.text = String.format("%.2f", (get.distance / time)) + " min/km"
+            binding.tvDateValue.text = SimpleDateFormat("dd-MM-yy").format(Date(firstTime))
         }
 
         fun setMapLocation(){
             if(!::map.isInitialized) return
+
+            //Build zoom level
+            val builder = LatLngBounds.builder()
+            builder.include(path.first())
+            builder.include(path.last())
+            val bounds = builder.build()
+            val padding = 75
+
             with(map){
-                moveCamera(CameraUpdateFactory.newLatLngZoom(path[0], 13f))
+                //moveCamera(CameraUpdateFactory.newLatLngZoom(path[0], 13f))
+                moveCamera(CameraUpdateFactory.newLatLngBounds(bounds, padding))
                 mapType = GoogleMap.MAP_TYPE_HYBRID
-                val options = PolylineOptions().addAll(path).color(Color.GREEN)
+
+                val pathColor = ContextCompat.getColor(binding.root.context, R.color.run_path)
+                val options = PolylineOptions().addAll(path).color(pathColor)
                 map.addPolyline(options)
                 // setOnMapClickListener {}
             }
